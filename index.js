b8553a0ab3e96e33e8a5536cc869809ac5180443
Vatsal Patel <69387444+vatsal-git@users.noreply.github.com>
2021-01-31 22:30:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+
+var numberOfDrumBtn = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumBtn; ++i) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        play(this.innerHTML);
+        keyPressAnimation(this.innerHTML);
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+
+    play(event.key);
+    keyPressAnimation(event.key);
+});
+
+
+function play(key) {
+
+    switch (key) {
+
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "j":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        default: console.log("this.innerHTMl");
+            break;
+
+    }
+}
+
+function keyPressAnimation(eventKey) {
+
+    document.querySelector("." + eventKey).classList.add("pressed");
+
+    setTimeout(function () {
+        document.querySelector("." + eventKey).classList.remove("pressed");
+    }, 100);
+
+}
